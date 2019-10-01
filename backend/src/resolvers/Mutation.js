79275d1051d1c38895945d9fdf7f16a93364ea9d
Melinda Golden <info@melindagolden.com>
2019-10-01 11:20:57 -0400

    Create items

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,13 @@
-const mutations = {};
+const mutations = {
+  async createItem(parent, args, ctx, info) {
+    const item = await ctx.db.mutation.createItem({
+      data: {
+        ...args,
+      },
+    }, info);
+
+    return item;
+  },
+};
 
 module.exports = mutations;
