fb0f6bc1f5cbb6ef15fa2f6843c904bd3d1f3eab
Melinda Golden <info@melindagolden.com>
2019-10-07 10:30:31 -0400

    Send email backend

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -2,6 +2,7 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const { randomBytes } = require('crypto');
 const { promisify } = require('util');
+const { transport, makeANiceEmail } = require('../mail');
 
 const mutations = {
   async createItem(parent, args, ctx, info) {
@@ -73,21 +74,29 @@ const mutations = {
     ctx.response.clearCookie('token');
     return { message: 'Goodbye!' };
   },
-  async requestReset(parent, { email }, ctx, info) {
+  async requestReset(parent, args, ctx, info) {
     // 1. Check if this is a real user
-    const user = await ctx.db.query.user({ where: { email } });
-    if (!user) throw new Error(`No such user found for email ${email}`);
+    const user = await ctx.db.query.user({ where: { email: args.email } });
+    if (!user) throw new Error(`No such user found for email ${args.email}`);
 
     // 2. Set a reset token and expiry on that user
     const randomBytesPromiseified = promisify(randomBytes);
     const resetToken = (await randomBytesPromiseified(20)).toString('hex');
     const resetTokenExpiry = Date.now() + 3600000; // 1 hour from now
     const res = await ctx.db.mutation.updateUser({
-      where: { email },
+      where: { email: args.email },
       data: { resetToken, resetTokenExpiry },
     });
 
     // 3. Email them that reset token
+    const mailRes = await transport.sendMail({
+      from: 'info@melindagolden.com',
+      to: user.email,
+      subject: 'Your Password Reset Token',
+      html: makeANiceEmail(`Your Password Reset Token is here!
+      \n\n
+      <a href="${process.env.FRONTEND_URL}/reset?resetToken=${resetToken}">Click Here to Reset</a>`),
+    });
 
     // 4. Return the message
     return { message: 'Thanks!' };
