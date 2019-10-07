57fbc8834fb3094dd553a4b1e553fd29ab389b57
Melinda Golden <info@melindagolden.com>
2019-10-07 15:10:40 -0400

    Updated Table styling

--- frontend/components/styles/Table.js
+++ frontend/components/styles/Table.js
@@ -3,16 +3,16 @@ import styled from 'styled-components';
 const Table = styled.table`
   border-spacing: 0;
   width: 100%;
-  border: 1px solid ${props => props.theme.offWhite};
+  border: 1px solid ${(props) => props.theme.offWhite};
   thead {
     font-size: 10px;
   }
   td,
   th {
-    border-bottom: 1px solid ${props => props.theme.offWhite};
-    border-right: 1px solid ${props => props.theme.offWhite};
-    padding: 10px 5px;
+    border-bottom: 1px solid ${(props) => props.theme.offWhite};
+    border-right: 1px solid ${(props) => props.theme.offWhite};
     position: relative;
+    padding: 5px;
     &:last-child {
       border-right: none;
       width: 150px;
@@ -20,10 +20,14 @@ const Table = styled.table`
         width: 100%;
       }
     }
+    label {
+      display: block;
+      padding: 10px 5px;
+    }
   }
   tr {
     &:hover {
-      background: ${props => props.theme.offWhite};
+      background: ${(props) => props.theme.offWhite};
     }
   }
 `;
