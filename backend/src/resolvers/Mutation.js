9b54d286707fea6d582d81779440eb7a644c3610
Melinda Golden <info@melindagolden.com>
2019-10-08 09:52:05 -0400

    Remove from cart backend

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -235,6 +235,20 @@ const mutations = {
       info,
     );
   },
+  async removeFromCart(parent, args, ctx, info) {
+    const where = { id: args.id };
+    // 1. Find the cart item
+    const cartItem = await ctx.db.query.cartItem({ where }, '{ id, user { id }}');
+
+    // 1.5 Make sure we found an item
+    if (!cartItem) throw new Error('No CartItem Found!');
+
+    // 2. Make sure they own that cart item
+    if (cartItem.user.id !== ctx.request.userId) throw new Error('Cheatin huhhhh');
+
+    // 3. Delete that cart item
+    return ctx.db.mutation.deleteCartItem({ where }, info);
+  },
 };
 
 module.exports = mutations;
