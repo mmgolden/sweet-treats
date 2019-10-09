e21ab3235e2aa4db9c44caa667f50aee8ee16225
Melinda Golden <info@melindagolden.com>
2019-10-09 16:36:43 -0400

    Added rating component

--- frontend/components/Item.js
+++ frontend/components/Item.js
@@ -6,42 +6,47 @@ import PriceTag from './styles/PriceTag';
 import formatMoney from '../lib/formatMoney';
 import DeleteItem from './DeleteItem';
 import AddToCart from './AddToCart';
+import Rating from './Rating';
 
-const Item = ({
-  item: {
+const Item = ({ item }) => {
+  const {
     id,
     title,
     price,
     description,
     image,
-  },
-}) => (
-  <ItemStyles>
-    {image && <img src={image} alt={title} />}
-    <Title>
-      <Link href={{
-        pathname: '/item',
-        query: { id },
-      }}
-      >
-        <a>{title}</a>
-      </Link>
-    </Title>
-    <p>{description}</p>
-    <PriceTag>{formatMoney(price)}</PriceTag>
-    <div className="buttonList">
-      <Link href={{
-        pathname: '/update',
-        query: { id },
-      }}
-      >
-        <a>Edit</a>
-      </Link>
-      <AddToCart id={id} />
-      <DeleteItem id={id}>Delete item</DeleteItem>
-    </div>
-  </ItemStyles>
-);
+    ratings,
+  } = item;
+
+  return (
+    <ItemStyles>
+      {image && <img src={image} alt={title} />}
+      <Title>
+        <Link href={{
+          pathname: '/item',
+          query: { id },
+        }}
+        >
+          <a>{title}</a>
+        </Link>
+      </Title>
+      <p>{description}</p>
+      <PriceTag>{formatMoney(price)}</PriceTag>
+      <Rating itemId={id} ratings={ratings} />
+      <div className="buttonList">
+        <Link href={{
+          pathname: '/update',
+          query: { id },
+        }}
+        >
+          <a>Edit</a>
+        </Link>
+        <AddToCart id={id} />
+        <DeleteItem id={id}>Delete item</DeleteItem>
+      </div>
+    </ItemStyles>
+  );
+};
 
 Item.propTypes = {
   item: PropTypes.shape({
---
+++ frontend/components/Rating.js
@@ -0,0 +1,51 @@
+import { useMutation } from '@apollo/react-hooks';
+import styled from 'styled-components';
+import Select from './Select';
+import CREATE_RATING_MUTATION from '../graphql/mutations/createRating';
+
+const ratingOptions = [1, 2, 3, 4, 5];
+
+const Rating = ({
+  itemId,
+  ratings,
+}) => {
+  const [createRating] = useMutation(CREATE_RATING_MUTATION);
+
+  const ratingTotal = ratings.reduce((total, { rating }) => total + rating, 0);
+  const ratingAverage = (ratingTotal / ratings.length).toFixed(2);
+
+  return (
+    <RatingStyles>
+      <p>{`Rating: ${ratings.length > 0 ? `${ratingAverage}/5` : '0'}`}</p>
+      <Select
+        name="ratingSelect"
+        label="Rate item:"
+        defaultValue={5}
+        options={ratingOptions}
+        handleChange={(e) => {
+          const inputRating = parseFloat(e.target.value);
+          createRating({
+            variables: {
+              id: itemId,
+              rating: inputRating,
+            },
+          });
+        }}
+      />
+    </RatingStyles>
+  );
+};
+
+const RatingStyles = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-bottom: 10px;
+  p {
+    flex-grow: unset;
+    &:first-child {
+      margin-right: 10px;
+    }
+  }
+`;
+
+export default Rating;
