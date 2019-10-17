f32657bb8b8416afe150d783bb604d0ff15b1ee3
Melinda Golden <info@melindagolden.com>
2019-10-17 12:20:34 -0400

    Added tests for Item

--- frontend/__tests__/Item.test.js
+++ frontend/__tests__/Item.test.js
@@ -1,9 +1,11 @@
+import '@testing-library/jest-dom/extend-expect'
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, queryByAltText } from '@testing-library/react';
 import { ThemeProvider } from 'styled-components';
 import ItemComponent from '../components/Item';
 import theme from '../components/styles/theme';
 import { ApolloMockedProvider } from '../test-utils/providers';
+import formatMoney from '../lib/formatMoney';
 
 const fakeItem = {
   id: '12345',
@@ -15,13 +17,42 @@ const fakeItem = {
   ratings: [5],
 };
 
-test('<Item />', async () => {
-  const wrapper = render(
-    <ApolloMockedProvider>
-      <ThemeProvider theme={theme}>
-        <ItemComponent item={fakeItem} />
-      </ThemeProvider>
-    </ApolloMockedProvider>,
-  );
-  wrapper.debug();
+describe('<Item />', () => {
+  it('renders the image', () => {
+    const { debug, queryByAltText } = render(
+      <ApolloMockedProvider>
+        <ThemeProvider theme={theme}>
+          <ItemComponent item={fakeItem} />
+        </ThemeProvider>
+      </ApolloMockedProvider>,
+    );
+    debug();
+    const img = queryByAltText(fakeItem.title);
+    expect(img).toHaveAttribute('alt', fakeItem.title);
+    expect(img).toHaveAttribute('src', fakeItem.image);
+  });
+
+  it('renders the title and price', () => {
+    const { queryByText } = render(
+      <ApolloMockedProvider>
+        <ThemeProvider theme={theme}>
+          <ItemComponent item={fakeItem} />
+        </ThemeProvider>
+      </ApolloMockedProvider>,
+    );
+    const title = queryByText(fakeItem.title);
+    expect(title).toHaveTextContent(fakeItem.title);
+    const price = queryByText(formatMoney(fakeItem.price));
+    expect(price).toHaveTextContent(formatMoney(fakeItem.price));
+  });
+
+  it('renders the rating', () => {
+    const { queryByText } = render(
+      <ApolloMockedProvider>
+        <ThemeProvider theme={theme}>
+          <ItemComponent item={fakeItem} />
+        </ThemeProvider>
+      </ApolloMockedProvider>,
+    );
+  });
 });
